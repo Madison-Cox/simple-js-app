7bdb97505e0aad3ee11c57f97c645ae5cff672c4
Madison-Cox <madison.cox5285@hotmail.com>
2022-08-10 15:48:56 -0500

    update scripts.js
    Removed excessive curly brackets, added loop that iterates name with the pokemons height.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,17 @@ let pokemonList = [
     {
         name: 'Charmander',
         height: '.6',
-        type: 'fire'
-    }
+        type: 'fire'},
     {
         name: 'Mudkip',
         height: '.4',
-        type: 'water'
-    }
+        type: 'water'},
     {
         name: 'Bulbasaur',
         height: '.7',
-        type: ['grass','poison']
-    }
+        type: ['grass','poison']},
 ];
+// loop that iterates name with height
+for (let i=0; i < pokemonList.length; i++) {
+        document.write(pokemonList[i].name +  "(Height:  )" + pokemonList[i].height +);
+}
