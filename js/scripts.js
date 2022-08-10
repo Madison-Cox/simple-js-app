d0a5139821db3ab4e6da1aab08164eafa4893f27
Madison-Cox <madison.cox5285@hotmail.com>
2022-08-10 10:14:28 -0500

    Added pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello World');
-let favoriteFood = 'steak';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Charmander',
+        height: '.6',
+        type: 'fire'
+    }
+    {
+        name: 'Mudkip',
+        height: '.4',
+        type: 'water'
+    }
+    {
+        name: 'Bulbasaur',
+        height: '.7',
+        type: ['grass','poison']
+    }
+];
