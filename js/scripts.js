3d3304541311855149894350970087f524a8c702
Madison-Cox <madison.cox5285@hotmail.com>
2022-08-11 08:43:25 -0500

    Corrected placement of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -14,13 +14,11 @@ let pokemonList = [
 ];
 // loop that iterates name with height
 // added a conditional
-for (let i=0; i < pokemonList.length; i++) {
-        document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")");
-if (pokemonList[i].height >=.7) {
-            document.write('This is a bigger pokemon!');
-        }
-else if (pokemonList[i].height <.5) {
-            document.write('This is a smaller pokemon!');
-        }
-
+for (let i = 0; i < pokemonList.length; i++){
+  if (pokemonList[i].height >=.7) {
+            document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') - This is a bigger pokemon!')
+  }
+else if (pokemonList[i].height <.7) {
+  document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ')')
+}
 }
